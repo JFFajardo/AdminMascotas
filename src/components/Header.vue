6ac5e8a69bfb15654fb432c48d073dddcf7cda0a
Johan Fajardo <johan.fajardo@gmail.com>
2023-06-11 00:34:10 -0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+   <h1 class="font-black text-5xl text-center">
+    Seguimiento a Pacientes
+    <span class="text-indigo-600 block">Veterinaria Mis Peludos Locos</span>
+   </h1>
+</template>
